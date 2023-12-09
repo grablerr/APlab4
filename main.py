4fa5b5fe7883ba30035161113ae4fa6e91d2a34a
Nikita <ppdbavyss@vk.com>
2023-12-09 18:59:44 +0400

    add realization for task of counting words

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import os
+import nltk
+import re
+
+def count_words(text):
+    words = nltk.word_tokenize(text)
+    words_only = [word for word in words if re.match(r"^\w+$", word)]
+    return len(words_only)
 
 def read_review(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -27,5 +34,7 @@ df = pd.DataFrame(data)
 
 df.fillna('ПУСТОЙ ОТЗЫВ', inplace=True)
 
-print(df)
+df['Количество слов'] = df['Текст рецензии'].apply(count_words)
+
+print(df.head())
 df.to_csv('data.csv', index=False)
